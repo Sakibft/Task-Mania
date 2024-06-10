dc70dcc5020120466ecbb5db1551e2ca4da2b7ab
Sakib <ssar96540@gmail.com>
2024-06-10 12:05:29 +0600

    added some data in the my submission sectionn worker dashboard

--- src/pages/Worker/WorkerSubmit.jsx
+++ src/pages/Worker/WorkerSubmit.jsx
@@ -15,15 +15,24 @@ const WorkerSubmit = () => {
   console.log(submitedTask);
   return (
     <div>
-      <div className="container mx-auto border rounded-xl shadow-xl">
+      <div className="container mx-auto border rounded-xl shadow-xl w-[1100px]">
           <div className="overflow-x-auto">
             <table className="table">
               {/* head */}
               <thead className="text-lg text-black">
                 <tr className="border rounded-xl border-b-pink-400">
+                  <th></th>
                   <th>Task Title</th>
+                  <th>Creator Name</th>
                   <th>Creator Email</th>
+                  <th> Worker Name</th>
+                  <th> Worker Email
+                  </th>
+                  <th>Detail</th>
                   <th>Amount</th>
+                  <th>date</th>
+                  <th>Current Date</th>
+                  <th>Task Id</th>
                   <th>Status</th>
                 </tr>
               </thead>
@@ -32,9 +41,17 @@ const WorkerSubmit = () => {
                  {
                   submitedTask && submitedTask.map(task => (
                     <tr key={task._id}>
+                    <td>{task?.image}</td>
                     <td>{task?.taskTitle}</td>
+                    <td>{task?.creatorName}</td>
                     <td>{task?.creatorEmail}</td>
+                    <td>{task?.workerName}</td>
+                    <td>{task?.workerEmail}</td>
+                    <td>{task?.detail}</td>
                     <td>{task?.amount}</td>
+                    <td>{task?.date}</td>
+                    <td>{task?.currentDate}</td>
+                    <td>{task?.taskId}</td>
                     <td>{task?.status}...</td>
                    </tr>
                   ))
