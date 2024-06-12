7d688bf4b2d17eb3f99f7ed6f7fa17e97677cfa0
Sakib <ssar96540@gmail.com>
2024-06-12 08:55:26 +0600

    added Enviroment variable

--- src/firebase/fConfig.js
+++ src/firebase/fConfig.js
@@ -1,23 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAic_8FAPvXDO2DPvIaWr17WByQellYX5Q",
-  authDomain: "taskmania-85588.firebaseapp.com",
-  projectId: "taskmania-85588",
-  storageBucket: "taskmania-85588.appspot.com",
-  messagingSenderId: "1099452318600",
-  appId: "1:1099452318600:web:6efb581ba31f638f5e703d",
-  measurementId: "G-77JE7R6THQ"
+ const firebaseConfig = {
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
+  measurementId:import.meta.env.VITE_measurementId
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth ;
